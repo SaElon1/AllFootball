f322bba8b5cdb652bea19cd45427a7c633987fc9
saelon <saelon@utu.fi>
2024-07-06 15:49:02 +0300

    signuplogin page started

--- frontend/src/pages/SignupLogin.jsx
+++ frontend/src/pages/SignupLogin.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './css/SignupLogin.css'
 
 
  const SignupLogin = () => {
   return (
-    <div>SignupLogin</div>
+    <div className='signuplogin'>
+      <div className="signuplogin-container">
+        <h1>Sign Up</h1>
+        <div className="signuplogin-fields">
+          <input type='text' placeholder='Name'></input>
+          <input type='text' placeholder='Email'></input>
+          <input type='text' placeholder='Password'></input>
+        </div>
+        <button>Sign up</button>
+        <p className="signuplogin-login">Already have an account?
+        <span>Login here</span>
+        </p>
+
+
+      </div>
+    </div>
   )
 }
 
