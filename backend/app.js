030733980521892755e489a93dd201c71afa0f46
saelon <saelon@utu.fi>
2024-07-21 17:36:46 +0300

    image upload configuration with multer

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,26 @@ app.get("/", (req, res) =>{
     res.send("Backend is running!")
 })
 
+//Image storing configuration
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./upload/images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname)
+    }
+})
+
+const upload = multer({storage})
+
+app.use("/images", express.static("upload/images"))
+app.post("/upload", upload.single("product"),(req,res) => {
+    res.json({
+        imageUrl: `http://localhost:${config.PORT}/images/${req.file.filename}`    })
+})
+
+
+
 app.use(express.json())
 app.use(cors())
 
