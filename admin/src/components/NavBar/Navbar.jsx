449e820d2c33c840757ba36b4832ace20625090a
saelon <saelon@utu.fi>
2024-07-23 14:37:30 +0300

    added css to admin navbar

--- admin/src/components/NavBar/Navbar.jsx
+++ admin/src/components/NavBar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useRef } from 'react'
 import logo from '../../assets/Logo.png'
 import { Link } from 'react-router-dom'
+import './NavBar.css'
 
 const Navbar = () => {
     const [menu, setMenu] = useState('addproducts')
     const menuref = useRef()
 
   return (
-    <div className='NavBar'>
+    <div className='navbar'>
         <div className="nav-logo"><img src={logo} alt="" /></div>
         <ul ref={menuref} className='nav-menu'>
             <li onClick={() => setMenu('addproducts')}><Link style={{textDecoration: 'none'}} to='/addproducts'>Add Products</Link>{menu ==="addproducts"?<hr/>:<></>}</li>
