f7bd44826481c07be3638f63a3152ab3475649bf
saelon <saelon@utu.fi>
2024-07-23 17:05:33 +0300

    size added to schema

--- admin/src/components/Addproducts/Addproducts.jsx
+++ admin/src/components/Addproducts/Addproducts.jsx
@@ -2,7 +2,65 @@ import React from 'react'
 
 const Addproducts = () => {
   return (
-    <div>Addproducts</div>
+    <div>
+      <h1>Add a new product to the database</h1>
+      <form className='addproducts-form'>
+        <div>
+          <p>Product Name</p>
+        <input type="text" placeholder='Name'></input>
+        </div>
+        <div>
+          <p>Description for the product</p>
+        <input type="text" placeholder='Description'></input>
+        </div>
+        <div>
+          <p>Product category</p>
+          <select name="category" className='addproduct-selector'>
+            <option value="Shirts">Shirts</option>
+            <option value="Shoes">Shoes</option>
+            <option value="Socks&Shirts">Socks & Shirts</option>
+            <option value="Balls&Pads">Balls & Padsß</option>
+          </select>
+        </div>
+        <div>
+          <p>Select size</p>
+          <select name="size" className='addproduct-selector'>
+          <option value="XS">XS</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="34">34</option>
+            <option value="35">35</option>
+            <option value="36">36</option>
+            <option value="37">37</option>
+            <option value="38">38</option>
+            <option value="39">39</option>
+            <option value="40">40</option>
+            <option value="41">41</option>
+            <option value="42">42</option>
+            <option value="43">43</option>
+            <option value="44">44</option>
+            <option value="45">45</option>
+          </select>
+        </div>
+        <div>
+          <p>Original price</p>
+        <input type="text" placeholder='Original price'></input>
+        </div>
+        <div>
+          <p>Offer price</p>
+          <input type="text" placeholder='Offer price'></input>
+        </div>
+        <div>
+          <p>Upload images of the product</p>
+        <input type="file" multiple></input>
+        </div>
+        <div className="addproducts-submitButton">
+          <button type='submit'>Add Product</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- admin/src/components/NavBar/Navbar.jsx
+++ admin/src/components/NavBar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div className="nav-logo"><img src={logo} alt="" /></div>
         <ul ref={menuref} className='nav-menu'>
             <li onClick={() => setMenu('addproducts')}><Link style={{textDecoration: 'none'}} to='/addproducts'>Add Products</Link>{menu ==="addproducts"?<hr/>:<></>}</li>
-            <li onClick={() => setMenu('allproducts')}><Link style={{textDecoration: 'none'}} to='/allproducts'>All Products</Link>{menu ==="allproducts"?<hr/>:<></>}</li>
+            <li onClick={() => setMenu('allproducts')}><Link style={{textDecoration: 'none'}} to='/allproducts'>Product List</Link>{menu ==="allproducts"?<hr/>:<></>}</li>
         </ul>
 
     </div>
