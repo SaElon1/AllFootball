793da1debc9124e913c6c9d990df16d09002e221
saelon <saelon@utu.fi>
2024-08-02 14:49:53 +0300

    user schema created

---
+++ backend/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String
+    },
+    email:{
+        type: String,
+        unique:true
+    },
+    password:{
+        type:String
+    },
+    cartItems:{ 
+        type: Object
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
+
