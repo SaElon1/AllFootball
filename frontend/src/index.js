875d57ad5be5e75a35e4ad5131045e75f2018d07
saelon <saelon@utu.fi>
2024-08-12 13:29:46 +0300

    Implemented userContext

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
+import UserContextProvider from './Context/UserContext'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <UserContextProvider>
   <ShopContextProvider>
     <App/>
   </ShopContextProvider>
+  </UserContextProvider>
 
 );
 
