9ad9466b254645ca355338efd3b5d2cec97d71c5
saelon <saelon@utu.fi>
2024-08-15 16:43:25 +0300

    created orders controller

---
+++ backend/controllers/orders.js
@@ -0,0 +1,39 @@
+const orderRouter = require('express').Router()
+const Order = require('../models/order')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = req => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.startsWith('Bearer')) {
+        return authorization.replace('Bearer', '')
+    }
+    return null
+}
+
+orderRouter.post('/order', async(req, res) => {
+    const {products, totalPrice} = req.body
+
+    const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+    if (!decodedToken.id) {
+        return res.status(401).json({error: 'Token invalid'})
+    }
+
+    try {
+        const user = await User.findById(decodedToken.id)
+
+    const newOrder = new Order({
+        products:products,
+        date: Date.now,
+        totalPrice:totalPrice
+    })
+
+
+    const savedOrder = await newOrder.save()
+    user.orders = user.orders.concat(newOrder._id)
+    await user.save()
+    res.status(200).json(savedOrder)
+    }catch(error){
+        console.error(error)
+    }
+})
