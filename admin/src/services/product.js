b9daef60de6147bca8b7d348331de86cb5c41e7b
saelon <saelon@utu.fi>
2024-09-09 11:22:40 +0300

    getProducts function

--- admin/src/services/product.js
+++ admin/src/services/product.js
@@ -21,7 +21,40 @@ const create = async (newObject) => {
     return response.data;
 }
 
-export default {create}
+const getProducts = async () => {
+    try {
+        const response = await axios.get(`${baseurl}/allproducts`)
+        return response.data
+    }catch(error){
+        console.error('Error in fetching allproducts')
+    }
+}
+
+const updateProduct = async (id, newObject) => {
+    const formData = new FormData()
+
+    for (let key in newObject) {
+        if(key === 'images'){
+            for (let i = 0; i < newObject.images.length; i++) {
+                formData.append('images', newObject.images[i])
+            }
+        }else {
+            formData.append(key, newObject[key])
+        }
+    }
+    try {
+        const response = await axios.put(`${baseurl}/${id}`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        return response.data
+    }catch(error){
+        console.error(error)
+    }
+}
+
+export default {create, getProducts, updateProduct}
 
 
 
