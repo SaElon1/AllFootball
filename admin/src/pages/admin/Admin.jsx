db886515d9b1497fcb3a547d8e8e6d927c1d9a95
saelon <saelon@utu.fi>
2024-09-12 12:48:49 +0300

    added frontpage to routes

--- admin/src/pages/admin/Admin.jsx
+++ admin/src/pages/admin/Admin.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Addproducts from '../../components/Addproducts/Addproducts'
 import Allproducts from '../../components/Allproducts/Allproducts'
+import FrontPage from '../../components/FrontPage/FrontPage'
 
 const Admin = () => {
   return (
     <div className='admin'>
       <Routes>
+        <Route path="/" element={<FrontPage/>}></Route>
         <Route path="/addproducts" element={<Addproducts/>}></Route>
         <Route path="/allproducts" element={<Allproducts/>}></Route>
       </Routes>
