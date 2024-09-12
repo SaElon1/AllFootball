d4ea4b2cf8ba43dee9df3269d4184b3803969c0b
saelon <saelon@utu.fi>
2024-09-12 12:48:31 +0300

    added main to navbar

--- admin/src/components/NavBar/Navbar.jsx
+++ admin/src/components/NavBar/Navbar.jsx
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom'
 import './NavBar.css'
 
 const Navbar = () => {
-    const [menu, setMenu] = useState('addproducts')
+    const [menu, setMenu] = useState('frontpage')
     const menuref = useRef()
 
   return (
     <div className='navbar'>
         <div className="nav-logo"><img src={logo} alt="" /></div>
         <ul ref={menuref} className='nav-menu'>
+        <li onClick={() => setMenu('frontpage')}><Link style={{textDecoration: 'none'}} to='/'>Main</Link>{menu ==="frontpage"?<hr/>:<></>}</li>
             <li onClick={() => setMenu('addproducts')}><Link style={{textDecoration: 'none'}} to='/addproducts'>Add Products</Link>{menu ==="addproducts"?<hr/>:<></>}</li>
             <li onClick={() => setMenu('allproducts')}><Link style={{textDecoration: 'none'}} to='/allproducts'>Product List</Link>{menu ==="allproducts"?<hr/>:<></>}</li>
         </ul>
