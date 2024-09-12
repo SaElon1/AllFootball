7f07c09717aa9da980063e1e0a744b195a9a3bb8
saelon <saelon@utu.fi>
2024-09-12 13:31:52 +0300

    frontpage to admin

---
+++ admin/src/components/FrontPage/FrontPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FrontPage.css'
+
+const FrontPage = () => {
+  return (
+    <div className='frontpage'>
+        <h1>Welcome to the Admin page</h1>
+    </div>
+  )
+}
+
+export default FrontPage
